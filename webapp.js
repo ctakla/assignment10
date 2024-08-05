65247607f7ba1774fafe25fee21c5a1ffd13f2d2
Christina <124402806+ctakla@users.noreply.github.com>
2024-08-05 16:16:09 -0400

    Update webapp.js

--- webapp.js
+++ webapp.js
@@ -3,7 +3,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'views')));
@@ -59,6 +58,7 @@ app.post('/process', async (req, res) => {
     }
 });
 
+const port = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });

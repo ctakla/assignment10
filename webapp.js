1c4100906fdcbe4d91c2fe4716aafa94c1b07d59
Christina <124402806+ctakla@users.noreply.github.com>
2024-08-05 15:59:30 -0400

    Update webapp.js

--- webapp.js
+++ webapp.js
@@ -1,64 +1,65 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const path = require('path');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-const uri = 'mongodb+srv://taklatina:pepsimaxi1!@cluster0.r41lzxh.mongodb.net/';
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'views')));
+
+const uri = 'mongodb+srv://taklatina:pepsimaxi1!@cluster0.r41lzxh.mongodb.net/Assignment10?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 async function main() {
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-
     try {
         await client.connect();
-        console.log("Connected to MongoDB");
-
-        const db = client.db('Assignment10');  // Replace with your database name
-        const collection = db.collection('places');  // Replace with your collection name
-
-        // Home route
-        app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, 'views', 'home.html'));
-        });
-
-        // Process route
-        app.post('/process', async (req, res) => {
-            const input = req.body.input;
-            let query;
-
-            if (/^\d/.test(input)) {
-                query = { zipCode: input };
-            } else {
-                query = { place: input };
-            }
-
-            try {
-                const results = await collection.find(query).toArray();
-                res.render('results', { results });
-            } catch (err) {
-                console.error("Error fetching data:", err);
-                res.status(500).send("Internal Server Error");
-            }
-        });
-
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
+        console.log('Connected to MongoDB');
     } catch (err) {
-        console.error(err);
-        process.exit(1);
+        console.error('Connection error:', err);
     }
 }
 
-main().catch(console.error);
+main();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/process', async (req, res) => {
+    const query = req.body.query;
+    try {
+        const db = client.db('Assignment10');
+        const collection = db.collection('places');
+        
+        let place;
+        if (isNaN(query.charAt(0))) {
+            place = await collection.findOne({ place: query });
+        } else {
+            place = await collection.findOne({ zips: query });
+        }
+        
+        if (place) {
+            res.render('results', { place: place.place, zips: place.zips });
+        } else {
+            res.render('results',{place: null, zips:[] });
+        }
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
